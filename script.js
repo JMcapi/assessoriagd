064b678bff3836690c3adbd4c50624a52464554d
João Marcelo <115075712+JMcapi@users.noreply.github.com>
2023-01-05 14:23:06 -0300

    commit 12

--- script.js
+++ script.js
@@ -133,6 +133,12 @@ addEventListener("load", type(document.querySelector("#titleGD")));
   
   //SCROLL SUAVE -->
 
+  function mascara(telefone){ 
+    if(telefone.value.length == 0)
+        telefone.value = '(' + telefone.value;
 
+    if(telefone.value.length == 3)
+        telefone.value = telefone.value + ') '; 
 
- 
+    if(telefone.value.length == 10)
+        telefone.value = telefone.value + '-';}
